73d9308d8a61af6f07346f8fcaae980c23c7933d
AndyZhuWei <zhuwei927@163.com>
2019-01-12 21:58:28 +0800

    Update ThreadLocalTest.java

--- src/main/java/com/example/demo/thread/ThreadLocalTest.java
+++ src/main/java/com/example/demo/thread/ThreadLocalTest.java
@@ -5,6 +5,7 @@ package com.example.demo.thread;
  * @Date:2018/12/18 10:34
  * @Description: 从这段代码的输出结果可以看出，在main线程中和thread1线程中，longLocal保存的副本值和stringLocal保存的副本值都不一样。
  * 最后一次在main线程再次打印副本值是为了证明在main线程中和thread1线程中的副本值确实是不同的。
+ * 参考 https://www.cnblogs.com/dolphin0520/p/3920407.html
  */
 public class ThreadLocalTest {
     ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
