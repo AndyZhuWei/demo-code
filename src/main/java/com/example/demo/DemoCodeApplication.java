28c5cce6f97cec88c1006a76134e14b8d2d6b4af
zhuwei <weizhu@healthlink.cn>
2020-11-30 17:02:03 +0800

    zookeeper

--- src/main/java/com/example/demo/DemoCodeApplication.java
+++ src/main/java/com/example/demo/DemoCodeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.example.demo.mapper")
+//@MapperScan("com.example.demo.mapper")  test
 public class DemoCodeApplication {
 
 	public static void main(String[] args) {
