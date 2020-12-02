81d816ec1a4fd0ad31c4fb4f1265b14183c9dc6f
zhuwei <weizhu@healthlink.cn>
2020-12-02 09:42:56 +0800

    master分支修改

--- src/main/java/com/example/demo/DemoCodeApplication.java
+++ src/main/java/com/example/demo/DemoCodeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.example.demo.mapper")  test   fdasfd
+//@MapperScan("com.example.demo.mapper")  test   fdasfd master分支修改
 public class DemoCodeApplication {
 
 	public static void main(String[] args) {
